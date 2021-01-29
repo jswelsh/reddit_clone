57966265ad3b06be21adf88195d9dfeb8fe23561
jswelsh <james.sinclair.welsh@gmail.com>
2021-01-28 17:01:42 -0800

    add cache work, have to setup a janky fix for non normalized cache of urql

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,13 +1,46 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
-import { createClient, Provider } from 'urql';
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql';
+import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache';
 import theme from '../theme'
+import { MeDocument, MeQuery } from '../generated/graphql';
 
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r:Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, data => fn(result, data as any) as any)
+}
 
+//create the urql client
 const client = createClient({
   url: 'http://localhost:4000/graphql',
   fetchOptions: {
     credentials:'include'
-  }
+  },
+  exchanges: [dedupExchange, cacheExchange({
+    updates: {
+      Mutation: {
+        login: (result, args, cache, info) => {
+          betterUpdateQuery<LoginMutation, MeQuery>(
+            cache,
+            { query: MeDocument },
+            result,
+            (result, query) => {
+              if (result.login.errors) {
+                return query
+              } else {
+                return {
+                  me: result.login.user
+                }
+              }
+            }
+            )
+        },
+      },
+    }
+  }), fetchExchange],
 });
 
 
