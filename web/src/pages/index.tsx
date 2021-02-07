083e5d9c2a907224f93d5f9a19622a6a996d6937
jswelsh <james.sinclair.welsh@gmail.com>
2021-02-07 03:56:41 -0800

    add pagination work for frontend

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,26 +1,63 @@
 import { withUrqlClient } from "next-urql"
 import NextLink from "next/link"
-import React from "react"
+import React, { useState } from "react"
 import { Layout } from "../components/Layout"
-import { Link } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Link, Stack, Text } from '@chakra-ui/react';
 import { usePostsQuery } from "../generated/graphql"
 import { createUrqlClient } from "../utils/createUrqlClient"
 
 const Index = () =>{
-  const [{data}] = usePostsQuery({
-    variables: {
-      limit: 10
-    }
+  const [variables,setVariables] = useState({
+    limit: 33,
+    cursor: null as null | string
   })
+  const [{data}] = usePostsQuery({
+    variables
+  })
+
+  if(!data){
+    return <div> uh oh, there are no posts. This maybe and us problem.</div>
+  }
+
 return (
   <Layout>
-    <NextLink href="/create-post">
-      <Link>
-        create post
-      </Link>
-    </NextLink>
+    <Flex align="center">
+      <Heading>LiReddit</Heading>
+      <NextLink href="/create-post">
+        <Link ml="auto">
+          create post
+        </Link>
+      </NextLink>
+    </Flex>
+
     <br />
-    {!data ? null : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
+    {!data
+      ? <div>loading...</div>
+      : (
+        <Stack>
+          {data!.posts.posts.map((p) =>( 
+          // <div key={p.id}>{p.title}</div>
+          <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+            <Heading fontSize="xl">{p.title}</Heading>
+            <Text mt={4}>{p.textSnippet}</Text>
+          </Box>
+          ))}
+        </Stack>
+        )}
+        {data && data.posts.hasMore ? (
+          <Flex>
+            <Button onClick={() =>{
+              setVariables({
+                limit:variables.limit,
+                cursor: data.posts.posts[data.posts.posts.length -1].createdAt
+              })
+            }}
+            m="auto"
+            my={8}>
+              load more
+            </Button>
+          </Flex>
+        ): null}
   </Layout>
   )
 }
